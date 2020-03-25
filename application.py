a4de732e43ac8db4bbd30abbec07c7076f511795
QAutoBotOP <m.monster137@protonmail.com>
2020-03-25 20:42:41 +0100

    Added new objectives

--- application.py
+++ application.py
@@ -1,11 +1,35 @@
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     headline = "Oi, govna!"
+#     return render_template("index.html", headline=headline)
+
+# @app.route("/<string:name>")
+# def hello(name):
+#     return f"Hello, {name}!"
+
+# import datetime
+
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     now = datetime.datetime.now()
+#     new_year = now.month == 1 and now.day == 1
+#     # text = "Yes" if new_year else "NO"
+#     return render_template("index.html", new_year=new_year)
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-@app.route("/<string:name>")
-def hello(name):
-    return f"Hello, {name}!"
+    names = ["Aelis", "Benedict", "Charles"]
+   return render_template("index.html", names=names)
